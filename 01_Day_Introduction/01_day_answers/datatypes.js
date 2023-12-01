8acdff231d95bc5ab504fd08382f99709ecc7f85
Frank South <frank@franksouth.dev>
2023-12-01 13:11:07 -0500

    Completed Day 1 exercises

---
+++ 01_Day_Introduction/01_day_answers/datatypes.js
@@ -0,0 +1,9 @@
+// const isString = 'True';
+// const isBool = true;
+// let isUndefined;
+// const isNull = null;
+
+// console.log(typeof isString);
+// console.log(typeof isBool);
+// console.log(typeof isUndefined);
+// console.log(typeof isNull);
---
+++ 01_Day_Introduction/01_day_answers/variable.js
@@ -0,0 +1,39 @@
+// Declare variables and assign string, boolean, undefined, and null data types
+
+const isString = 'True';
+const isBool = true;
+let isUndefined;
+const isNull = '';
+
+// Declare four variables without assigning values
+
+let randomNum;
+let food;
+var todayDate;
+let stateFlags;
+
+// Declare four variables with assigned values
+
+const PI = 3.14;
+const name = 'Frank';
+let calories = 575;
+let favoriteAnimal = 'Penguin';
+
+// Declare variables to store your first name, last name, marital status, country and age in multiple lines
+
+const firstName = 'Frank';
+const lastName = 'South';
+const married = true;
+const age = 33;
+
+// Declare variables to store your first name, last name, marital status, country and age in a single line
+
+// const firstName = 'Frank', lastName = 'South', married = true, age = 33;
+
+// Declare two variables myAge and yourAge and assign them inital values and log to the browser console.
+
+const myAge = 33;
+const yourAge = 25;
+
+console.log(`I am ${myAge} years old.`)
+console.log(`You are ${yourAge} years old.`)
